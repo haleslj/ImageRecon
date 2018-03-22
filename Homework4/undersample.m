8b879cdeb8e60886c3c3693ad832f54568bff0e0
haleslj <laurel.hales@gmail.com>
2018-03-22 13:26:06 -0600

    Improved
    Corrected the fft2 function to include the appropriate fftshifts.

--- Homework4/undersample.m
+++ Homework4/undersample.m
@@ -1,14 +1,15 @@
-function [ ima ] = undersample( im, Rx, Ry )
+function [ ima,ku ] = undersample( im, Rx, Ry )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 [Nx, Ny, L] = size(im);
 
-IM = fft2(im); 
+IM = fftshift(fft2(fftshift(im))); 
 IM1 = zeros(size(IM));
 
 idx = 1:Rx:Nx;
 idy = 1:Ry:Ny;
 IM1(idx,idy,:) = IM(idx,idy,:);
-ima = ifft2(IM1); 
+ima = ifft2(IM1);
+ku = IM1;
 end
 
